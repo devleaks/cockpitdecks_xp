68c93f71d0988cfa557b68dbc12c433d4998f9de
Pierre M <6030167+devleaks@users.noreply.github.com>
2024-10-03 18:46:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cockpitdecks Decks
+
+This is the implementation files for
+ - Stream Decks
+ - Loupedeck
+ - Berhinger X-Touch Mini
+
+ These files are used by the Cockpitdecks application
+ to implement phydsical decks connections.
+ 
---
+++ cockpitdecks_xp/__init__.py
@@ -0,0 +1,10 @@
+#
+# COCKPITDECKS EXTENSIONS: X-Plane Simulator
+#
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # Aum
+from datetime import datetime
+
+__NAME__ = "cockpitdecks_xp"
+__COPYRIGHT__ = f"© 2022-{datetime.now().strftime('%Y')} Pierre M <pierre@devleaks.be>"
+
+__version__ = "1.0.0"
