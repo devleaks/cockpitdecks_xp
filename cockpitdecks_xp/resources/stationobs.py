3b9b59acbc6e7346d146cc0a587ad5b39b381434
Pierre M <6030167+devleaks@users.noreply.github.com>
2025-02-18 10:41:48 +0100

    write weather station icao to variable

--- cockpitdecks_xp/resources/stationobs.py
+++ cockpitdecks_xp/resources/stationobs.py
@@ -1,10 +1,15 @@
+# Custom observable that transform (lat, lon) into the the ICAO code
+# of the closest weather station (real life, fetch by AVWX).
+# The ICAO code of the closest station is written to weather-station Cockpitdecks variable.
+# WEATHER_STATION_MONITORING = "weather-station"
+#
 import logging
 from datetime import datetime, timedelta
 
 from avwx import Station
 
+from cockpitdecks import WEATHER_STATION_MONITORING
 from cockpitdecks.observable import Observable
-from cockpitdecks.variable import Variable
 from cockpitdecks.simulator import Simulator, SimulatorVariable
 
 logger = logging.getLogger(__name__)
@@ -13,6 +18,7 @@ logger.setLevel(logging.DEBUG)
 
 LATITUDE = "sim/flightmodel/position/latitude"
 LONGITUDE = "sim/flightmodel/position/longitude"
+COORDINATES = {LATITUDE, LONGITUDE}
 
 
 class WeatherStationObservable(Observable):
@@ -22,13 +28,12 @@ class WeatherStationObservable(Observable):
     """
 
     DEFAULT_STATION = "EBBR"
-    WEATHER_STATION_VARIABLE = "weather-station-icao"
 
     def __init__(self, simulator: Simulator):
         wso_config = {
             "name": "weather station observable",
             "multi-datarefs": [LATITUDE, LONGITUDE],
-            "actions": [{"set_dataref": self.WEATHER_STATION_VARIABLE}],  # Action is specific, in our case: (lat, lon) -> weather station icao
+            "actions": [{}],  # Action is specific, in our case: (lat, lon) -> weather station icao
         }
         super().__init__(config=wso_config, simulator=simulator)
         self.check_time = 30  # seconds
@@ -36,14 +41,19 @@ class WeatherStationObservable(Observable):
         self._last_updated = datetime.now()
         self._no_coord_warn = 0
         self._value.update_value(new_value=self.DEFAULT_STATION)
-        logger.debug(f"changed station to {self.DEFAULT_STATION}")
+        self._set_dataref = simulator.get_variable(name=SimulatorVariable.internal_variable_name(path=WEATHER_STATION_MONITORING), is_string=True)
+        self._set_dataref.update_value(new_value=self.DEFAULT_STATION, cascade=True)
+        logger.debug(f"set initial station to {self.DEFAULT_STATION}")
 
     def get_variables(self) -> set:
-        return {LATITUDE, LONGITUDE}
+        return COORDINATES
 
     def simulator_variable_changed(self, data: SimulatorVariable):
         if (datetime.now() - self._last_checked).seconds < self.check_time:
-            return
+            return  # too early to change
+
+        if data.name not in COORDINATES:
+            return  # not for me, should never happen
 
         lat = self.sim.get_simulator_variable_value(LATITUDE)
         lon = self.sim.get_simulator_variable_value(LONGITUDE)
@@ -56,8 +66,11 @@ class WeatherStationObservable(Observable):
         self._last_checked = datetime.now()
         (nearest, coords) = Station.nearest(lat=lat, lon=lon, max_coord_distance=150000)
         if nearest.icao != self._value.value():
-            logger.debug(f"changed station to {nearest.icao} ({lat}, {lon})")
-            self._value.update_value(new_value=nearest.icao, cascade=True)
+            logger.debug(f"changed station to {nearest.icao} ({round(lat, 6)}, {round(lon, 6)})")
+            self.value = nearest.icao
+            print(">>> before set", self._set_dataref.name, nearest.icao)
+            self._set_dataref.update_value(new_value=nearest.icao, cascade=True)
             self._last_updated = datetime.now()
+            print(">>> after set", self._set_dataref.name, nearest.icao)
         else:
-            logger.debug(f"checked, no change")
+            logger.debug("checked, no change")
